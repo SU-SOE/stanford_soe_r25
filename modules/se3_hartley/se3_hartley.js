fb11fbe30d4494a634ad1d64a5acacd5e8ca6950
Bill Minor <minorwm@stanford.edu>
2018-06-13 10:28:29 -0500

    SOE-3110: Adding SWS module structure for release (SCSS/CSS etc...)

--- modules/se3_hartley/se3_hartley.js
+++
@@ -1,6 +0,0 @@
-// change the link associated with the Hartley site logo to return to Stanford Earth main site.
-(function ($) {
-    $(document).ready(function () {
-        $("#logo a").attr("href","https://earth.stanford.edu/");
-    });
-})(jQuery);
