9a84cb4b20f6959f361ebe831ce769bff53fbdef
Bill Minor <minorwm@stanford.edu>
2018-06-19 15:54:25 -0500

    SOE-3111: Adding some js and css/scss

--- js/script.js
+++ js/script.js
@@ -3,9 +3,29 @@
   /*
    *  My script code comment
    */
-  Drupal.behaviors.stanford_module_example = {
+  Drupal.behaviors.stanford_soe_r25 = {
     attach: function (context) {
       // My code here...
+      $('#edit-submitted-event-date-month').on('change', function (e) {
+        $('#edit-submitted-event-date-month').show();
+        $('.webform-component--event-date .webform-datepicker').addClass('picked');
+      });
+      $('#edit-submitted-event-date-day').on('change', function (e) {
+        $('#edit-submitted-event-date-day').show();
+      });
+      $('#edit-submitted-event-date-year').on('change', function (e) {
+        $('#edit-submitted-event-date-year').show();
+      });
+      $('#edit-submitted-event-date-finish-month').on('change', function (e) {
+        $('#edit-submitted-event-date-finish-month').show();
+        $('.webform-component--event-date-finish .webform-datepicker').addClass('picked');
+      });
+      $('#edit-submitted-event-date-finish-day').on('change', function (e) {
+        $('#edit-submitted-event-date-finish-day').show();
+      });
+      $('#edit-submitted-event-date-finish-year').on('change', function (e) {
+        $('#edit-submitted-event-date-finish-year').show();
+      });
     }
   };
 
