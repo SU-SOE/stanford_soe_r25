040eeb262ea2291c6ee200ac75ee7d6abeff80f1
Bill Minor <minorwm@stanford.edu>
2018-06-19 17:31:52 -0500

    SOE-3111: Adding some js and css/scss

--- js/script.js
+++ js/script.js
@@ -7,24 +7,12 @@
     attach: function (context) {
       // My code here...
       $('#edit-submitted-event-date-month').on('change', function (e) {
-        $('#edit-submitted-event-date-month').show();
+        $('.webform-component--event-date .webform-datepicker select').show();
         $('.webform-component--event-date .webform-datepicker').addClass('picked');
       });
-      $('#edit-submitted-event-date-day').on('change', function (e) {
-        $('#edit-submitted-event-date-day').show();
-      });
-      $('#edit-submitted-event-date-year').on('change', function (e) {
-        $('#edit-submitted-event-date-year').show();
-      });
       $('#edit-submitted-event-date-finish-month').on('change', function (e) {
-        $('#edit-submitted-event-date-finish-month').show();
+        $('.webform-component--event-date-finish .webform-datepicker select').show();
         $('.webform-component--event-date-finish .webform-datepicker').addClass('picked');
-      });
-      $('#edit-submitted-event-date-finish-day').on('change', function (e) {
-        $('#edit-submitted-event-date-finish-day').show();
-      });
-      $('#edit-submitted-event-date-finish-year').on('change', function (e) {
-        $('#edit-submitted-event-date-finish-year').show();
       });
     }
   };
