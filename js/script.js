23d6b20aab5bd0f960b58d2c44d7412a866cad13
Bill Minor <minorwm@stanford.edu>
2018-07-13 17:40:18 -0500

    Adding changes to js

--- js/script.js
+++ js/script.js
@@ -6,13 +6,14 @@
   Drupal.behaviors.stanford_soe_r25 = {
     attach: function (context) {
       // Show the date select fields after a user chooses a date on old reservation form
-      $('#edit-submitted-event-date-month').on('change', function (e) {
-        $('.webform-component--event-date .webform-datepicker select').show();
-        $('.webform-component--event-date .webform-datepicker').addClass('picked');
-      });
-      $('#edit-submitted-event-date-finish-month').on('change', function (e) {
-        $('.webform-component--event-date-finish .webform-datepicker select').show();
-        $('.webform-component--event-date-finish .webform-datepicker').addClass('picked');
+      $('#edit-submitted-event-date-month, #edit-submitted-event-date-finish-month').on('change', function (e) {
+        if($(this.id).selector == 'edit-submitted-event-date-month') {
+          $('.webform-component--event-date .webform-datepicker select').show();
+          $('.webform-component--event-date .webform-datepicker').addClass('picked');
+        } else if ($(this.id).selector == 'edit-submitted-event-date-finish-month') {
+          $('.webform-component--event-date-finish .webform-datepicker select').show();
+          $('.webform-component--event-date-finish .webform-datepicker').addClass('picked');
+        }
       });
     }
   };
