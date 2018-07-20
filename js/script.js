804dd9d84c8b511719efa3db9988337d1a642960
Bill Minor <minorwm@stanford.edu>
2018-07-19 19:07:12 -0500

    SOE-3256: Didn't add context to all the selectors. Fixing

--- js/script.js
+++ js/script.js
@@ -8,11 +8,11 @@
       // Show the date select fields after a user chooses a date on old reservation form
       $('#edit-submitted-event-date-month, #edit-submitted-event-date-finish-month', context).on('change', function (e) {
         if($(this.id).selector == 'edit-submitted-event-date-month') {
-          $('.webform-component--event-date .webform-datepicker select').show();
-          $('.webform-component--event-date .webform-datepicker').addClass('picked');
+          $('.webform-component--event-date .webform-datepicker select', context).show();
+          $('.webform-component--event-date .webform-datepicker', context).addClass('picked');
         } else if ($(this.id).selector == 'edit-submitted-event-date-finish-month') {
-          $('.webform-component--event-date-finish .webform-datepicker select').show();
-          $('.webform-component--event-date-finish .webform-datepicker').addClass('picked');
+          $('.webform-component--event-date-finish .webform-datepicker select', context).show();
+          $('.webform-component--event-date-finish .webform-datepicker', context).addClass('picked');
         }
       });
     }
